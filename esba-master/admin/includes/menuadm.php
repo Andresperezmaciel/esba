2e4818b324f6265d0ea83bf399d77a1e0a37cded
lGera03 <169067617+lGera03@users.noreply.github.com>
2024-06-04 15:21:47 -0300

    Add files via upload

---
+++ esba-master/admin/includes/menuadm.php
@@ -0,0 +1,9 @@
+<div class="list-group text-center">
+                        <button type="button" class="list-group-item list-group-item-action"><a href="altausuario.php">Alta de Usuarios</a></button>
+                        <button type="button" class="list-group-item list-group-item-action"><a href="bajaymodifusuario.php">Baja/Modificación de Usuarios</a></button>
+                        <br>
+                        <button type="button" class="list-group-item list-group-item-action"><a href="altamarca.php">Marcas</a></button>
+                        <button type="button" class="list-group-item list-group-item-action"><a href="altatipo_producto.php">Tipo de Producto</a></button>
+                        <button type="button" class="list-group-item list-group-item-action"><a href="altaproducto.php">Producto</a></button>
+                    </div>
+                </div>
---
+++ esba-master/admin/includes/validaraltamarca.php
@@ -0,0 +1,24 @@
+<?php
+    include("configadm.php");
+    session_start();
+    error_reporting(0);
+
+    if(isset($_SESSION['usuario'])) {
+        if(isset($_POST['registrar'])){
+            $marca = $_POST['marca'];
+
+            $query = mysqli_query($conexion,"INSERT INTO marca(nombre) VALUES('$marca')");
+        }
+
+        if($query){
+            echo "<script>alert('Marca registrada correctamente!');</script>";
+            header("Location:http://localhost/esba-master/admin/altamarca.php");
+        }else{
+            echo "<script>alert('¡No pudo registrarse por un problema!');</script>";
+            header("Location:http://localhost/esba-master/admin/altamarca.php");
+        }
+    }else {
+        header("Location:index.php");
+        exit;
+    }
+?>
