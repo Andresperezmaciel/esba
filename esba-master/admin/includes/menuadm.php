32c75073ca523ee98c721b9e6799dc179097f1fd
lGera03 <169067617+lGera03@users.noreply.github.com>
2024-06-05 12:35:29 -0300

    Add files via upload

---
+++ esba-master/admin/includes/menuadm.php
@@ -0,0 +1,9 @@
+<div class="list-group text-center">
+                        <button type="button" class="list-group-item list-group-item-action"><a href="altausuario.php">Alta de Usuarios</a></button>
+                        <button type="button" class="list-group-item list-group-item-action"><a href="bajaymodifusuario.php">Baja/Modificación de Usuarios</a></button>
+                        <br>
+                        <button type="button" class="list-group-item list-group-item-action"><a href="marca.php">Marcas</a></button>
+                        <button type="button" class="list-group-item list-group-item-action"><a href="tipo_producto.php">Tipo de Producto</a></button>
+                        <button type="button" class="list-group-item list-group-item-action"><a href="altaproducto.php">Alta de Productos</a></button>
+                    </div>
+                </div>
